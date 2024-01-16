4d5c5e675f6cc272d4981bbfde0bd4df708a4507
Leadstoyou <lytieulong2j2@gmail.com>
2024-01-16 15:02:29 +0700

    add root folder for course detail

---
+++ src/app/pages/course-detail/paid-course-detail.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const PaidCourseDetail = (props: Props) => {
+  return <div>unpaid-course-detail.page</div>;
+};
+
+export default PaidCourseDetail;
