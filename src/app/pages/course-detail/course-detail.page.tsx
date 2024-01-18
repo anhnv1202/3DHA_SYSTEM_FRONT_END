bdac47c031b7aa31ee48d8531895a284b45e98ea
Leadstoyou <lytieulong2j2@gmail.com>
2024-01-18 13:05:01 +0700

    complete template for course detail

--- src/app/pages/course-detail/course-detail.page.tsx
+++ src/app/pages/course-detail/course-detail.page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import UnpaidCourseDetail from './unpaid-course-detail.page';
 import PaidCourseDetail from './paid-course-detail.component';
 
-type Props = { isUnpaidPage?: boolean };
+type Props = { isPaidPage?: boolean };
 
-const CourseDetail = ({ isUnpaidPage }: Props) => {
-  return isUnpaidPage ? <UnpaidCourseDetail /> : <PaidCourseDetail />;
+const CourseDetail = ({ isPaidPage }: Props) => {
+  return isPaidPage ? <PaidCourseDetail /> : <UnpaidCourseDetail />;
 };
 
 export default CourseDetail;
